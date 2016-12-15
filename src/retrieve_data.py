d9b5d9d578bbc1b68df073f712f2c7e494cef4a2
Real Limoges <b.real.limoges@gmail.com>
2016-12-14 16:44:32 -0800

    update .gitignore; refactor and pylint retrieve_data

--- src/retrieve_data.py
+++ src/retrieve_data.py
@@ -1,20 +1,24 @@
+'''
+This module grabs data from Quandl.com using their REST API.
+'''
+
 import quandl
 import pandas as pd
 
-API_KEY = open(API_KEY).read().strip()
+API_KEY = open('API_KEY.txt').read().strip()
 
 BITCOIN = 'BCHARTS/BITSTAMPUSD'
 CLOSE = 'Close'
 RATE = 'Rate'
 US_10_YR = '10 YR'
 
-SOURCES = { 'GOOG/INDEXDJX_DJI': 'DJIA',
-            'USTREASURY/YIELD': 'US_10_YR',
-            'YAHOO/INDEX_RUT': 'RUS2000',
-            'YAHOO/INDEX_SSEC': 'SSEC',
-            'CURRFX/USDEUR' : 'EURO',
-            'CURRFX/USDJPY' : 'YEN',
-            'CURRFX/USDCHF' : 'SWISS'}
+SOURCES = {'GOOG/INDEXDJX_DJI': 'DJIA',
+           'USTREASURY/YIELD': 'US_10_YR',
+           'YAHOO/INDEX_RUT': 'RUS2000',
+           'YAHOO/INDEX_SSEC': 'SSEC',
+           'CURRFX/USDEUR': 'EURO',
+           'CURRFX/USDJPY': 'YEN',
+           'CURRFX/USDCHF': 'SWISS'}
 
 
 def generate_dataset():
@@ -22,40 +26,46 @@ def generate_dataset():
     Queries Quandl for information
     '''
 
-    df  = pd.DataFrame(quandl.get(BITCOIN, api_key=API_KEY)[CLOSE]).rename(
-                       columns={CLOSE: 'BTC'})
+    bitcoin_data = pd.DataFrame(quandl.get(BITCOIN,
+                                           api_key=API_KEY)[CLOSE]).rename(columns={CLOSE: 'BTC'})
 
-    for k,v  in SOURCES.iteritems():
-        new_data = quandl.get(k, api_key = API_KEY)
-        
-        if '10 YR' in new_data.columns:
-            new_data = pd.DataFrame(new_data[US_10_YR]).rename(
-                                    columns={US_10_YR: v})
-        elif 'Close' in new_data.columns:
-            new_data = pd.DataFrame(new_data[CLOSE]).rename(
-                columns={CLOSE: v})
+    for source_k, source_v in SOURCES.iteritems():
+        new_quandl = quandl.get(source_k, api_key=API_KEY)
 
-        elif 'Rate' in new_data.columns:
-            new_data = pd.DataFrame(new_data[RATE]).rename(
-                columns={RATE: v})
+        if '10 YR' in new_quandl.columns:
+            new_quandl = pd.DataFrame(new_quandl[US_10_YR]).rename(
+                columns={US_10_YR: source_v})
+        elif 'Close' in new_quandl.columns:
+            new_quandl = pd.DataFrame(new_quandl[CLOSE]).rename(
+                columns={CLOSE: source_v})
 
-        #Merges the new_data column onto the dataset
-        df = pd.merge(df, new_data, how='left', left_index=True,
-                      right_index=True)
-    
-    return df
+        elif 'Rate' in new_quandl.columns:
+            new_quandl = pd.DataFrame(new_quandl[RATE]).rename(
+                columns={RATE: source_v})
+
+        # Merges the new_quandl column onto the dataset
+        bitcoin_data = pd.merge(
+            bitcoin_data,
+            new_quandl,
+            how='left',
+            left_index=True,
+            right_index=True)
+
+    return bitcoin_data
 
 
-def interpolate(df, method='time'):
+def interpolate(col, method='time'):
     '''
     Interpolates the data
     '''
 
-    for col in df.columns:
-        df[col] = df[col].interpolate(method=method)
+    return col.interpolate(method=method)
 
-    return df
-    
+
 if __name__ == '__main__':
-    df = interpolate( generate_dataset() ).to_csv('../data/dataset.csv')
-    
+    full_dataset = generate_dataset()
+
+    for feature_col in full_dataset.columns:
+        full_dataset[feature_col] = interpolate(full_dataset[feature_col])
+
+    full_dataset.to_csv('../dataset.csv')
